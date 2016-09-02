f7943e2fdaab4d46008782f5de50274a3dcaed65
Helton Uema <heltonuema@gmail.com>
2016-09-01 21:53:20 -0300

    Outro teste

--- app/src/main/java/br/iesb/contatospos/LoginActivity.java
+++ app/src/main/java/br/iesb/contatospos/LoginActivity.java
@@ -50,7 +50,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
-            // teste
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
