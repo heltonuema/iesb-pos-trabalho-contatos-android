ecdbbc2f8e41e5b174a4ee3468c2096af268d380
Helton Uema <heltonuema@gmail.com>
2016-09-05 22:20:47 -0300

    -Funcionalidade da tela de login -Funcionalidade da tela de cadastro -Alteracao no estilo do layout

--- app/src/main/java/br/iesb/contatospos/activity/LoginActivity.java
+++ app/src/main/java/br/iesb/contatospos/activity/LoginActivity.java
@@ -57,6 +57,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
+
+
     private static final int REQUEST_CADASTRAR = 1;
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
