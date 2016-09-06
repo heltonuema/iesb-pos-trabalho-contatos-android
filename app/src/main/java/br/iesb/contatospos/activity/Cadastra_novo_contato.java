e825605517216f4588246ac915e259523fa0e95b
Andre <andre.murta@hotmai.com>
2016-09-05 23:51:32 -0300

    inclusao das activities listacontatos e cadastra_novo_contato

--- app/src/main/java/br/iesb/contatospos/activity/Cadastra_novo_contato.java
+++ app/src/main/java/br/iesb/contatospos/activity/Cadastra_novo_contato.java
@@ -17,7 +17,7 @@ public class Cadastra_novo_contato extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastra_novo_contato);
 
