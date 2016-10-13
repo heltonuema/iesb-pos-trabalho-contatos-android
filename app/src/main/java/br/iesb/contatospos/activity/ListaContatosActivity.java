a6f9dcbbedd0a84f3a6d464e08e819b8550879cc
Helton <heltonuema@gmail.com>
2016-10-13 00:09:26 -0300

    Inclusao do chat com firebase

--- app/src/main/java/br/iesb/contatospos/activity/ListaContatosActivity.java
+++ app/src/main/java/br/iesb/contatospos/activity/ListaContatosActivity.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.Icon;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
